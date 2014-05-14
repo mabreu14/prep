2fc12428132afb7dc3181b97c7c2afe293294d18
Menyueru <maddog28@gmail.com>
2014-05-14 17:26:37 -0400

    Pushing new changes

--- source/prep/learning_mspec/Calculator.cs
+++ source/prep/learning_mspec/Calculator.cs
@@ -1,9 +1,11 @@
-﻿namespace prep.learning_mspec
+﻿using System;
+namespace prep.learning_mspec
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if(first <0 || second <0) throw new ArgumentException();
 	    return first + second;
     }
   }
