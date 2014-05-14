44cebb904338b76f6f9eb1df4863f2a5000cc236
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-05-14 15:32:54 -0600

    Updated

--- source/prep.specs/CalculatorSpecs.cs
+++ source/prep.specs/CalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
@@ -12,17 +13,37 @@ namespace prep.specs
     {
     }
 
+    public class when_the_calculator_is_instantiated : concern_for_calculator
+    {
+      Establish c = () =>
+        connection = depends.on<IDbConnection>();
+
+      It should_not_have_opened_its_connection = () =>
+        connection.never_received(x => x.Open());
+
+      static IDbConnection connection;
+    }
+
     public class when_adding_two_numbers : concern_for_calculator
     {
       public class and_they_are_both_positive
       {
+        Establish c = () =>
+        {
+          connection = depends.on<IDbConnection>();          
+        };
+
         Because b = () =>
           result = sut.add(2, 3);
 
         It should_return_the_sum = () =>
           result.ShouldEqual(5);
 
+        It should_open_a_connection_to_the_database = () =>
+          connection.received(x => x.Open());
+
         static int result;
+        static IDbConnection connection;
       }
 
       public class and_there_are_negative_numbers_involved
