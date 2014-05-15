9909b61eb555a7343696b96180739c941e62053e
Maria <mabreu@intellisys.com.do>
2014-05-14 18:07:37 -0700

    Pushing new changes

--- source/prep/learning_mspec/Calculator.cs
+++ source/prep/learning_mspec/Calculator.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Data;
+using System.Security;
+using System.Security.Principal;
 
 namespace prep.learning_mspec
 {
   public class Calculator
   {
-    IDbConnection connection;
+        IDbConnection connection;
+        IPrincipal _principal;
 
-    public Calculator(IDbConnection connection, IDbConnection other)
+      public Calculator(IDbConnection connection, IPrincipal principal)
     {
-      this.connection = connection;
+        this.connection = connection;
+        _principal = principal;
     }
 
-    public int add(int first, int second)
+      public int add(int first, int second)
     {
       if (first < 0 || second < 0) throw new ArgumentException();
 
@@ -23,7 +27,9 @@ namespace prep.learning_mspec
 
     public void shut_off()
     {
-      throw new NotImplementedException();
+        throw new SecurityException();
+        if (!_principal.IsInRole("hello"))
+            throw new SecurityException();
     }
   }
 }
